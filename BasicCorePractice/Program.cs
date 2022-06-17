2e99bb1ffaf62b8e28fd7031480b73ad587aff28
gourab <gourab.saheb100@gmail.com>
2022-06-17 15:43:51 +0530

    ADD: SWAP the IRONMAN

--- BasicCorePractice/Program.cs
+++ BasicCorePractice/Program.cs
@@ -1,26 +1,10 @@
 ﻿
-Console.WriteLine("Flip Coin and print percentage of Heads and Tails");
+ Console.WriteLine("Program to Swap Two Numbers");
 
-int n, h_cnt = 0, t_cnt = 0;
-double heads, tails;
-// INPUT DATA
-Console.WriteLine("enter no. of times you want to flip the coin: ");
-n = Convert.ToInt16(Console.ReadLine());
-
-// COMPUTATION
-for (int j = 0; j < n; j++)
-{
-    Random r = new Random();
-    double random = r.Next(2);
-    if (random < 0.5)
-        t_cnt++;
-    else
-        h_cnt++;
-}
-
-heads = h_cnt / (double)n * 100;
-tails = t_cnt / (double)n * 100;
-
-Console.WriteLine("Percentage of heads: " + heads + "%");
-Console.WriteLine("Percentage of tails: " + tails + "%");
-
+int a = Convert.ToInt16(Console.ReadLine());
+int b = Convert.ToInt16(Console.ReadLine()); ;
+Console.WriteLine("Before swap a= " + a + " b= " + b);
+a = a + b;
+b = a - b;
+a = a - b;
+Console.Write("After swap a= " + a + " b= " + b);
