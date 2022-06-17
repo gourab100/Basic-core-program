4559320d4b8fd9140e14d8d2f33dca6175047450
gourab <gourab.saheb100@gmail.com>
2022-06-17 15:38:12 +0530

    ADD: I AM IRONMAN

--- BasicCorePractice/Program.cs
+++ BasicCorePractice/Program.cs
@@ -1,26 +1,18 @@
 ﻿
-Console.WriteLine("Flip Coin and print percentage of Heads and Tails");
 
-int n, h_cnt = 0, t_cnt = 0;
-double heads, tails;
-// INPUT DATA
-Console.WriteLine("enter no. of times you want to flip the coin: ");
-n = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Program to Check Whether a Number is Even or Odd");
 
-// COMPUTATION
-for (int j = 0; j < n; j++)
+
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a Number : ");
+
+if (i % 2 == 0)
 {
-    Random r = new Random();
-    double random = r.Next(2);
-    if (random < 0.5)
-        t_cnt++;
-    else
-        h_cnt++;
+    Console.Write("Entered Number is an Even Number");
+    Console.Read();
 }
-
-heads = h_cnt / (double)n * 100;
-tails = t_cnt / (double)n * 100;
-
-Console.WriteLine("Percentage of heads: " + heads + "%");
-Console.WriteLine("Percentage of tails: " + tails + "%");
+else
+{
+    Console.Write("Entered Number is an Odd Number");
+}
 
