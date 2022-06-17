a42b241cee5c2d4e2a648a949f22c096bac3194e
gourab <gourab.saheb100@gmail.com>
2022-06-17 15:41:28 +0530

    ADD: Alphabet vowel

--- BasicCorePractice/Program.cs
+++ BasicCorePractice/Program.cs
@@ -1,26 +1,20 @@
 ﻿
-Console.WriteLine("Flip Coin and print percentage of Heads and Tails");
+ Console.WriteLine("Program to Check Whether an Alphabet is Vowel or Consonant");
 
-int n, h_cnt = 0, t_cnt = 0;
-double heads, tails;
-// INPUT DATA
-Console.WriteLine("enter no. of times you want to flip the coin: ");
-n = Convert.ToInt16(Console.ReadLine());
+char ch;
 
-// COMPUTATION
-for (int j = 0; j < n; j++)
+Console.WriteLine("Enter any character: ");
+ch = Convert.ToChar(Console.ReadLine());
+
+
+// Condition for vowel checking
+if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
 {
-    Random r = new Random();
-    double random = r.Next(2);
-    if (random < 0.5)
-        t_cnt++;
-    else
-        h_cnt++;
+
+    Console.WriteLine(ch + " is Vowel.");
+
 }
-
-heads = h_cnt / (double)n * 100;
-tails = t_cnt / (double)n * 100;
-
-Console.WriteLine("Percentage of heads: " + heads + "%");
-Console.WriteLine("Percentage of tails: " + tails + "%");
-
+else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+{
+    Console.WriteLine(ch + " is Consonant.");
+}
